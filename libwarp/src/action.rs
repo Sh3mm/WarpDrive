533152b71b4d089b5f1cbeef6dfe501985f9e912
Sh3mm <sh3mm@github.com>
2024-08-02 09:49:29 -0400

    change the 'to_action' to an Action method named 'from' for better encapsulation

--- libwarp/src/action.rs
+++ libwarp/src/action.rs
@@ -65,14 +65,86 @@ impl Action {
             action: action_type
         }
     }
+
+    fn from(path: &String, local: &Option<OffsetDateTime>, remote: &Option<OffsetDateTime>, ledger: &Ledger) -> Self {
+        let last_update = &ledger.update_time;
+        match ledger.path_set.get(path) {
+            // The ledger does not hold the file. This means it's new and should be added to the right destination
+            // unless there are conflicting new files
+            None => {
+                // conflicting new files
+                if local.is_some() && remote.is_some() {
+                    return Self::new(path, ActionType::Error(ErrorType::TwoNew))
+                }
+                // new file in remote
+                if local.is_none() && remote.is_some() {
+                    return Self::new(path, ActionType::Remote2Local)
+                }
+                // new file in local
+                if local.is_some() && remote.is_none() {
+                    return Self::new(path, ActionType::Local2Remote)
+                }
+
+                else { panic!("Impossible scenario where a file exists but appears nowhere: \"{path}\"") }
+            }
+            // The ledger hold the file. This mean the action must be decided by comparing update times
+            Some(_) => {
+                // No local
+                if local.is_none() {
+                    let unwrap_remote = remote.unwrap();
+                    // (no lo) + (re <= lu) -> DelRemote
+                    if unwrap_remote.cmp(last_update).is_le() {
+                        return Self::new(path, ActionType::DelRemote)
+                    }
+                    // (no lo) + (re > lu) -> Error
+                    if unwrap_remote.cmp(last_update).is_gt() {
+                        return Self::new(path, ActionType::Error(ErrorType::DelAndMod))
+                    }
+                }
+                // remote is None
+                if remote.is_none() {
+                    let local_time = local.unwrap();
+                    // (lo <= lu) + (no re) -> DelLocal
+                    if local_time.cmp(last_update).is_le() {
+                        return Self::new(path, ActionType::DelLocal)
+                    }
+                    // (re > lu) + (no lo) -> Error
+                    if local_time.cmp(last_update).is_gt() {
+                        return Self::new(path, ActionType::Error(ErrorType::ModAndDel))
+                    }
+                }
+                let remote_time = remote.unwrap();
+                let local_time = local.unwrap();
+
+                // (re <= lu) + (lo > lu) -> Local2Remote
+                if remote_time.cmp(last_update).is_le() & local_time.cmp(last_update).is_gt() {
+                    return Self::new(path, ActionType::Local2Remote)
+                }
+                // (re > lu) + (lo <= lu) -> Local2Remote
+                if remote_time.cmp(last_update).is_gt() & local_time.cmp(last_update).is_le() {
+
+                    return Self::new(path, ActionType::Remote2Local)
+                }
+                // (re <= lu) + (lo <= lu) -> Nothing
+                if remote_time.cmp(last_update).is_le() & local_time.cmp(last_update).is_le() {
+                    return Self::new(path, ActionType::Nothing)
+                }
+                // (re > lu) + (lo > lu) -> Error
+                if remote_time.cmp(last_update).is_gt() & local_time.cmp(last_update).is_gt() {
+                    return Self::new(path, ActionType::Error(ErrorType::TwoSideMod))
+                }
+                else { panic!("impossible state reached") }
+            }
+        }
+    }
 }
 
 pub fn gen_action_list(local: &Vec<RFileInfo>, remote: &Vec<RFileInfo>, ledger: &Ledger) -> Vec<Action> {
     let file_map = create_file_map(local, remote);
     return file_map.iter().map(
-        |(p, times)| to_action(p ,&times[0], &times[1], &ledger)
+        |(p, times)| Action::from(p ,&times[0], &times[1], &ledger)
     ).collect::<Vec<Action>>()
-}
+}gen_action_list
 
 fn create_file_map(l1: &Vec<RFileInfo>, l2: &Vec<RFileInfo>) -> HashMap<String, [Option<OffsetDateTime>; 2]> {
     let list_chain = Iterator::chain(
@@ -90,76 +162,4 @@ fn create_file_map(l1: &Vec<RFileInfo>, l2: &Vec<RFileInfo>) -> HashMap<String, 
         map.insert(v.path.clone(), pair);
     }
     return map;
-}
-
-fn to_action(path: &String, local: &Option<OffsetDateTime>, remote: &Option<OffsetDateTime>, ledger: &Ledger) -> Action {
-    let last_update = &ledger.update_time;
-    match ledger.path_set.get(path) {
-        // The ledger does not hold the file. This means it's new and should be added to the right destination
-        // unless there are conflicting new files
-        None => {
-            // conflicting new files
-            if local.is_some() && remote.is_some() {
-                return Action::new(path, ActionType::Error(ErrorType::TwoNew))
-            }
-            // new file in remote
-            if local.is_none() && remote.is_some() {
-                return Action::new(path, ActionType::Remote2Local)
-            }
-            // new file in local
-            if local.is_some() && remote.is_none() {
-                return Action::new(path, ActionType::Local2Remote)
-            }
-
-            else { panic!("Impossible scenario where a file exists but appears nowhere: \"{path}\"") }
-        }
-        // The ledger hold the file. This mean the action must be decided by comparing update times
-        Some(_) => {
-            // No local
-            if local.is_none() {
-                let unwrap_remote = remote.unwrap();
-                // (no lo) + (re <= lu) -> DelRemote
-                if unwrap_remote.cmp(last_update).is_le() {
-                    return Action::new(path, ActionType::DelRemote)
-                }
-                // (no lo) + (re > lu) -> Error
-                if unwrap_remote.cmp(last_update).is_gt() {
-                    return Action::new(path, ActionType::Error(ErrorType::DelAndMod))
-                }
-            }
-            // remote is None
-            if remote.is_none() {
-                let local_time = local.unwrap();
-                // (lo <= lu) + (no re) -> DelLocal
-                if local_time.cmp(last_update).is_le() {
-                    return Action::new(path, ActionType::DelLocal)
-                }
-                // (re > lu) + (no lo) -> Error
-                if local_time.cmp(last_update).is_gt() {
-                    return Action::new(path, ActionType::Error(ErrorType::ModAndDel))
-                }
-            }
-            let remote_time = remote.unwrap();
-            let local_time = local.unwrap();
-
-            // (re <= lu) + (lo > lu) -> Local2Remote
-            if remote_time.cmp(last_update).is_le() & local_time.cmp(last_update).is_gt() {
-                return Action::new(path, ActionType::Local2Remote)
-            }
-            // (re > lu) + (lo <= lu) -> Local2Remote
-            if remote_time.cmp(last_update).is_gt() & local_time.cmp(last_update).is_le() {
-
-                return Action::new(path, ActionType::Remote2Local)
-            }
-            // (re <= lu) + (lo <= lu) -> Nothing
-            if remote_time.cmp(last_update).is_le() & local_time.cmp(last_update).is_le() {
-                return Action::new(path, ActionType::Nothing)
-            }
-            // (re > lu) + (lo > lu) -> Error
-            if remote_time.cmp(last_update).is_gt() & local_time.cmp(last_update).is_gt() {
-                return Action::new(path, ActionType::Error(ErrorType::TwoSideMod))
-            }
-            else { panic!("impossible state reached") }
-        }
-    }
 }
